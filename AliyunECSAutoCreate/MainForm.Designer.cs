01b2feb008385e7e5ced299824f38b8a39f27f4e
tanyang <tanyang2@hotmail.com>
2020-03-15 14:22:12 +0800

    1.0

---
+++ AliyunECSAutoCreate/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AliyunECSAutoCreate
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tm1 = new System.Windows.Forms.Timer(this.components);
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Enabled = false;
+            this.btn1.Location = new System.Drawing.Point(60, 82);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(110, 43);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "初始化";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(12, 31);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(411, 25);
+            this.tb1.TabIndex = 1;
+            // 
+            // tm1
+            // 
+            this.tm1.Interval = 1000;
+            this.tm1.Tick += new System.EventHandler(this.tm1_Tick);
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.Location = new System.Drawing.Point(234, 82);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(110, 43);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "更新解析";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 173);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "阿里云ECS自动创建";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Timer tm1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
---
+++ AliyunECSAutoCreate/MainForm.cs
@@ -0,0 +1,215 @@
+﻿using Aliyun.Acs.Alidns.Model.V20150109;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Ecs.Model.V20140526;
+using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AliyunECSAutoCreate
+{
+    public partial class MainForm : Form
+    {
+        private string accessKeyId = null;
+        private string accessSecret = null;
+        private string regionId = null;
+        private string instanceName = null;
+        private string launchTemplateName = null;
+        private string domainRecordId = null;
+        private string domainRecordRR = null;
+
+        private bool hasDomain = false;
+        private string instanceId = null;
+        private int checkCount = 0;
+        private DefaultAcsClient client = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BackgroundWork(() =>
+            {
+                accessKeyId = ConfigurationManager.AppSettings["AccessKeyId"];
+                accessSecret = ConfigurationManager.AppSettings["AccessSecret"];
+                regionId = ConfigurationManager.AppSettings["RegionId"];
+                instanceName = ConfigurationManager.AppSettings["InstanceName"];
+                launchTemplateName = ConfigurationManager.AppSettings["LaunchTemplateName"];
+                domainRecordId = ConfigurationManager.AppSettings["DomainRecordId"];
+                domainRecordRR = ConfigurationManager.AppSettings["DomainRecordRR"];
+
+                if (!string.IsNullOrEmpty(domainRecordId))
+                {
+                    hasDomain = true;
+                }
+
+                DefaultProfile profile = DefaultProfile.GetProfile(regionId, accessKeyId, accessSecret);
+                client = new DefaultAcsClient(profile);
+
+                CheckInstances();
+            });
+        }
+
+
+        void CheckInstances(bool loop = false)
+        {
+            var request = new DescribeInstancesRequest
+            {
+                RegionId = regionId,
+                InstanceName = instanceName
+            };
+            var response = client.GetAcsResponse(request);
+            var instance = response.Instances.FirstOrDefault();
+            if (instance != null && instance.PublicIpAddress.Any())
+            {
+                checkCount = 0;
+                instanceId = instance.InstanceId;
+                InvokeVoid(() =>
+                {
+                    tb1.Text = instance.PublicIpAddress.FirstOrDefault();
+                    btn1.Text = "释放";
+                    btn1.Enabled = true;
+                    if (hasDomain) btn2.Enabled = true;
+                });
+            }
+            else
+            {
+                if (loop)
+                {
+                    if (checkCount >= 10)
+                    {
+                        throw new Exception("未查询到服务器！");
+                    }
+                    else
+                    {
+                        checkCount++;
+                        StartTimer();
+                    }
+                }
+                else
+                {
+                    InvokeVoid(() =>
+                    {
+                        tb1.Text = "";
+                        btn1.Text = "创建";
+                        btn1.Enabled = true;
+                        btn2.Enabled = false;
+                    });
+                }
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+
+            BackgroundWork(() =>
+            {
+                if (string.IsNullOrEmpty(instanceId))
+                {
+                    var request = new RunInstancesRequest
+                    {
+                        RegionId = regionId,
+                        LaunchTemplateName = launchTemplateName,
+                        InstanceChargeType = "PostPaid"
+                    };
+                    var response = client.GetAcsResponse(request);
+                    if (!response.InstanceIdSets.Any())
+                    {
+                        throw new Exception("创建失败！");
+                    }
+
+                    StartTimer();
+                }
+                else
+                {
+                    var request = new DeleteInstanceRequest
+                    {
+                        RegionId = regionId,
+                        InstanceId = instanceId,
+                        Force = true
+                    };
+                    client.GetAcsResponse(request);
+                    instanceId = null;
+                    CheckInstances();
+                }
+            });
+        }
+
+        void BackgroundWork(Action action)
+        {
+            var bw = new BackgroundWorker();
+            bw.DoWork += (a, b) =>
+            {
+                try
+                {
+                    action?.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    InvokeVoid(() =>
+                    {
+                        tb1.Text = ex.Message;
+                        if (ex.Message.StartsWith("IncorrectInstanceStatus.Initializing"))
+                        {
+                            btn1.Enabled = true;
+                            btn2.Enabled = false;
+                        }
+                    });
+                }
+            };
+            bw.RunWorkerAsync();
+        }
+
+        void InvokeVoid(Action action)
+        {
+            this.Invoke(action);
+        }
+
+        private void tm1_Tick(object sender, EventArgs e)
+        {
+            tm1.Stop();
+            CheckInstances(true);
+        }
+
+        void StartTimer()
+        {
+            InvokeVoid(() =>
+            {
+                tm1.Start();
+            });
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            var ip = tb1.Text.Trim();
+            if (!string.IsNullOrEmpty(ip) && ip.Length < 16 && ip.Length > 6)
+            {
+                btn2.Enabled = false;
+
+                BackgroundWork(() =>
+                {
+                    var request = new UpdateDomainRecordRequest
+                    {
+                        RR = domainRecordRR,
+                        RecordId = domainRecordId,
+                        Type = "A",
+                        _Value = ip
+                    };
+                    client.GetAcsResponse(request);
+                    InvokeVoid(() =>
+                    {
+                        btn2.Enabled = true;
+                        Process.Start("cmd.exe", "/k ipconfig /flushdns");
+                    });
+                });
+            }
+        }
+    }
+}
